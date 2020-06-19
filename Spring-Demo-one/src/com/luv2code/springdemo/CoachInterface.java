567804a9736d4a5f01849b27a7ade61b84b6890a
JeetThakur <jbt9370@rit.edu>
2020-06-19 11:32:06 -0400

    delete

--- Spring-Demo-one/src/com/luv2code/springdemo/CoachInterface.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springdemo;
-
-// Only the method is mentioned in an interface not implemented
-public interface CoachInterface {
-	
-	public String getDailyWorkout();
-	public  String getDailyFortune();
-
-}
--- spring-demo-annotations/src/com/luv2code/springdemo/FortuneService.java
+++
@@ -1,8 +0,0 @@
-package com.luv2code.springdemo;
-
-public interface FortuneService {
-	
-	// Define the interface method
-	public String getFortune();
-
-}
